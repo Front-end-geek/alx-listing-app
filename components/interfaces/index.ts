da79db605261861fe9f20797ccb3ee196bc31efd
unknown <IamHendy>
2024-12-07 01:06:24 +0300

    changes

--- components/interfaces/index.ts
+++ components/interfaces/index.ts
@@ -1,11 +1,10 @@
 export interface CardProps {
-    title: string;
-    description: string;
-    imageUrl: string;
-  }
-  
-  export interface ButtonProps {
-    label: string;
-    onClick: () => void;
-  }
-  
+  title: string;
+  description: string;
+  imageUrl: string;
+}
+
+export interface ButtonProps {
+  label: string;
+  onClick: () => void;
+}
